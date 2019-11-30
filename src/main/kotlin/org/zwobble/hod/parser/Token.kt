5315ae5886d8d0d995a697cd8aa0c0a2d3005a2e
Michael Williamson <mike@zwobble.org>
2019-11-30 22:06:42 +0000

    Add RegexTokeniser

---
+++ src/main/kotlin/org/zwobble/hod/parser/Token.kt
@@ -0,0 +1,11 @@
+package org.zwobble.hod.parser
+
+internal data class Token<T>(val characterIndex: Int, val tokenType: T, val value: String) {
+    fun describe(): String {
+        return describeToken(tokenType, value)
+    }
+}
+
+internal fun <T> describeToken(tokenType: T, value: String) : String {
+    return "${tokenType}: ${value}"
+}
